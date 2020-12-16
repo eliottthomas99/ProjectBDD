1d2f98a6a2d91de71f63d4fa63ec8cf73faef752
Unknown <jthomas@enssat.fr>
2020-12-16 14:18:42 +0100

    Main V 2.15
    renouvelerEmprunt fini
    prise en compte du plafond de renouvellement
    prise en compte des demandes

--- gestionEmprunt.sql
+++ gestionEmprunt.sql
@@ -1,6 +1,7 @@
 USE bibliotheque;
 
 SET @plafondPenalite = 1;
+SET @maxRenouvellement = 3;
 
 DELIMITER $$
 
@@ -133,10 +134,16 @@ CREATE PROCEDURE renouvelerEmprunt(IN contenuValCodeBarre INT, contenuValNumeroL
 # permet de renouveler un emprunt si personne ne l a reserve
 RENOUVELEREMPRUNT_LABEL:BEGIN
 	# On commence par s assurer que le contenu est emprunte
-    IF (SELECT COUNT(*) FROM emprunt WHERE Contenu_Code_Barre=contenuValCodeBarre AND Contenu_Numero_License=contenuValNumeroLicense AND date_retour IS NULL) = 0 THEN 
-		LEAVE RENOUVELEREMPRUNT_LABEL;	
+    IF (SELECT COUNT(*) FROM emprunt WHERE Contenu_Code_Barre = contenuValCodeBarre AND Contenu_Numero_License = contenuValNumeroLicense AND date_retour IS NULL) = 0 THEN 
+		LEAVE RENOUVELEREMPRUNT_LABEL;
+	# On regarde si il y a une reservation pour le contenu
+	ELSEIF (SELECT COUNT(*) FROM demande WHERE Contenu_Code_Barre = contenuValCodeBarre AND Contenu_Numero_License = contenuValNumeroLicense) >= 1 THEN
+		LEAVE RENOUVELEREMPRUNT_LABEL;
+	# On regarde si l emprunt n a pas depasse le plafond de renouvellement
+	ELSEIF (SELECT renouvellement FROM emprunt WHERE Contenu_Code_Barre = contenuValCodeBarre AND Contenu_Numero_License = contenuValNumeroLicense AND date_retour IS NULL) >= @maxRenouvellement THEN
+		LEAVE RENOUVELEREMPRUNT_LABEL;
 	END IF;
-    # Si le contenu n est pas reserve et qu on est dans le dernier crenau, alors on peu etendre la reservation
+    # Si on est dans le dernier crenau de temps, alors on peu etendre l emprunt
 	SET @dateDebut = (SELECT date_pret FROM emprunt WHERE Contenu_Code_Barre = contenuValCodeBarre AND Contenu_Numero_License = contenuValNumeroLicense and date_retour IS NULL);
 	IF (SELECT support FROM contenu WHERE Code_Barre = contenuValCodeBarre AND Numero_License = contenuValNumeroLicense) = "livre" OR
 		(SELECT support FROM contenu WHERE Code_Barre = contenuValCodeBarre AND Numero_License = contenuValNumeroLicense) = "ebook" THEN
@@ -145,8 +152,12 @@ RENOUVELEREMPRUNT_LABEL:BEGIN
         IF CURDATE() >= @reservationBas AND CURDATE() <= @reservationHaut THEN
 			UPDATE emprunt SET renouvellement = renouvellement + 1 WHERE Contenu_Code_Barre = contenuValCodeBarre AND Contenu_Numero_License = contenuValNumeroLicense and date_retour IS NULL;
         END IF;
-	#ELSE
-	#	SET @delai = 7 * ((SELECT renouvellement FROM emprunt WHERE Contenu_Code_Barre = contenuValCodeBarre AND Contenu_Numero_License = contenuValNumeroLicense and date_retour IS NULL) + 1);
+	ELSE
+		SET @reservationBas = ADDDATE(@dateDebut, INTERVAL 7 * (SELECT renouvellement FROM emprunt WHERE Contenu_Code_Barre = contenuValCodeBarre AND Contenu_Numero_License = contenuValNumeroLicense and date_retour IS NULL) DAY);
+        SET @reservationHaut = ADDDATE(@dateDebut, INTERVAL 7 * ((SELECT renouvellement FROM emprunt WHERE Contenu_Code_Barre = contenuValCodeBarre AND Contenu_Numero_License = contenuValNumeroLicense and date_retour IS NULL) + 1) DAY);
+        IF CURDATE() >= @reservationBas AND CURDATE() <= @reservationHaut THEN
+			UPDATE emprunt SET renouvellement = renouvellement + 1 WHERE Contenu_Code_Barre = contenuValCodeBarre AND Contenu_Numero_License = contenuValNumeroLicense and date_retour IS NULL;
+        END IF;
 	END IF;
 
 END;
@@ -161,7 +172,7 @@ CALL emprunterContenu(40,0,12);
 CALL emprunterContenu(50,0,12);
 CALL emprunterContenu(0,60,12); # refuse l emprunt d un contenu car il y a deja 5 emprunts en cours
 
-CALL renouvelerEmprunt(10,0);
+#CALL renouvelerEmprunt(10,0);
 
 #CALL rendreContenu(0,60);
 
@@ -171,8 +182,13 @@ CALL renouvelerEmprunt(10,0);
 #UPDATE `bibliotheque`.`abonne` SET `penalite` = '0' WHERE (`numero` = '11');
 #CALL emprunterContenu(0,70,11); # ne devrait pas fonctionner car penalite trop haute
 
-# je rend en retard
+# Je rend en retard
 #CALL emprunterContenu(0,70,11);
 #UPDATE `bibliotheque`.`emprunt` SET `date_pret` = '2020-01-01' WHERE (`Contenu_Code_Barre` = '0000000000') and (`Contenu_Numero_License` = '0000000070') and (`Abonne_numero` = '11') and (`date_pret` = '2020-12-16');
 #CALL rendreContenu(0,70);
-CALL reserverContenuEmprunte(0 , 70,15);
+
+# Julien emprunte, Eliott reserve, Julien essaie d etendre sa reservation
+CALL emprunterContenu(0,70,11);
+CALL reserverContenuEmprunte(0 , 70,13);
+CALL renouvelerEmprunt(0,70);
+# Test OK, le renouvellement n est pas effectue
