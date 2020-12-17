6c6f5ec9fbc91398f0fd4f07e3389e85066c417c
Eliott <ethomas@enssat.fr>
2020-12-17 17:41:43 +0100

    Main V 3.5
    Etoffer jeu de données

--- insertionJeuDeDonnees.sql
+++ insertionJeuDeDonnees.sql
@@ -21,3 +21,89 @@ CALL ajoutDVD(17,"Nemo",10,"Andrew Stanton","Pixar","animation","Orange");
 
 
 /**********************************************************************************/
+
+
+
+CALL ajoutDVD(51,"Solo : a Star Wars Story",51,"Ron Howard","Kathleen Kennedy","aventure","IUT");
+CALL ajoutDVD(52,"Solo : a Star Wars Story",51,"Ron Howard","Kathleen Kennedy","aventure","IFSI");
+CALL ajoutDVD(53,"Solo : a Star Wars Story",51,"Ron Howard","Kathleen Kennedy","aventure","ENSSAT");
+CALL ajoutDVD(54,"Rogue One : a Star Wars Story",52,"Ron Howard","Kathleen Kennedy","aventure","ENSSAT");
+CALL ajoutDVD(55,"Rogue One : a Star Wars Story",52,"Ron Howard","Kathleen Kennedy","aventure","IUT");
+CALL ajoutDVD(56,"Rogue One : a Star Wars Story",52,"Ron Howard","Kathleen Kennedy","aventure","Nokia");
+CALL ajoutDVD(57,"Rogue One : a Star Wars Story",52,"Ron Howard","Kathleen Kennedy","aventure","Orange");
+CALL ajoutDVD(58,"Inception",53,"Christopher Nolan","Christopher Nolan","action","IUT");
+CALL ajoutDVD(59,"Inception",53,"Christopher Nolan","Christopher Nolan","action","ENSSAT");
+CALL ajoutDVD(60,"Inception",53,"Christopher Nolan","Christopher Nolan","action","Orange");
+CALL ajoutDVD(61,"Fight club",54,"David Fincher","Art Linson","drame","Orange");
+CALL ajoutDVD(62,"Fight club",54,"David Fincher","Art Linson","drame","ENSSAT");
+CALL ajoutDVD(63,"Fight club",54,"David Fincher","Art Linson","drame","IUT");
+CALL ajoutDVD(64,"Avengers",55,"Joss Whedon","Kevin Feige","action","Orange");
+CALL ajoutDVD(65,"Interstellar",56,"Christopher Nolan","Christopher Nolan","action","Orange");
+CALL ajoutDVD(66,"Interstellar",56,"Christopher Nolan","Christopher Nolan","action","IUT");
+CALL ajoutDVD(67,"Interstellar",56,"Christopher Nolan","Christopher Nolan","action","Nokia");
+
+
+CALL ajoutEBook(68,"Narnia",57,"C S Lewis","fantastique","Gallimard jeunesse","IUT");
+CALL ajoutEBook(69,"Narnia",57,"C S Lewis","fantastique","Gallimard jeunesse","ENSSAT");
+CALL ajoutEBook(70,"Narnia",57,"C S Lewis","fantastique","Gallimard jeunesse","IUT");
+CALL ajoutEBook(71,"Narnia",57,"C S Lewis","fantastique","Gallimard jeunesse","Nokia");
+CALL ajoutEBook(72,"Narnia",57,"C S Lewis","fantastique","Gallimard jeunesse","ENSSAT");
+CALL ajoutLivre(73,"Le rouge et le noir",58,"Stendhal","roman","Levasseur","IUT");
+CALL ajoutLivre(75,"Le rouge et le noir",58,"Stendhal","roman","Levasseur","ENSSAT");
+CALL ajoutLivre(76,"Le rouge et le noir",58,"Stendhal","roman","Levasseur","IFSI");
+CALL ajoutLivre(77,"Le rouge et le noir",58,"Stendhal","roman","Levasseur","IFSI");
+CALL ajoutLivre(78,"Harry Potter Episode I",59,"JK Rowling","fantastique","Gallimard jeunesse","ENSSAT");
+CALL ajoutLivre(79,"Harry Potter Episode I",59,"JK Rowling","fantastique","Gallimard jeunesse","IFSI");
+CALL ajoutLivre(80,"Harry Potter Episode I",59,"JK Rowling","fantastique","Gallimard jeunesse","IUT");
+CALL ajoutLivre(81,"Harry Potter Episode I",59,"JK Rowling","fantastique","Gallimard jeunesse","Nokia");
+CALL ajoutLivre(82,"Harry Potter Episode II",60,"JK Rowling","fantastique","Gallimard jeunesse","ENSSAT");
+CALL ajoutLivre(83,"Harry Potter Episode II",60,"JK Rowling","fantastique","Gallimard jeunesse","IUT");
+CALL ajoutLivre(84,"Harry Potter Episode II",60,"JK Rowling","fantastique","Gallimard jeunesse","IFSI");
+CALL ajoutLivre(85,"Harry Potter Episode II",60,"JK Rowling","fantastique","Gallimard jeunesse","Orange");
+CALL ajoutLivre(86,"Harry Potter Episode II",60,"JK Rowling","fantastique","Gallimard jeunesse","Nokia");
+CALL ajoutLivre(87,"Harry Potter Episode II",60,"JK Rowling","fantastique","Gallimard jeunesse","Orange");
+CALL ajoutLivre(88,"Harry Potter Episode III",61,"JK Rowling","fantastique","Gallimard jeunesse","ENSSAT");
+CALL ajoutLivre(89,"Harry Potter Episode III",61,"JK Rowling","fantastique","Gallimard jeunesse","IUT");
+CALL ajoutLivre(90,"Harry Potter Episode III",61,"JK Rowling","fantastique","Gallimard jeunesse","Orange");
+CALL ajoutLivre(91,"Harry Potter Episode III",61,"JK Rowling","fantastique","Gallimard jeunesse","IFSI");
+CALL ajoutLivre(92,"Harry Potter Episode III",61,"JK Rowling","fantastique","Gallimard jeunesse","Nokia");
+CALL ajoutLivre(93,"Harry Potter Episode IV",62,"JK Rowling","fantastique","Gallimard jeunesse","ENSSAT");
+CALL ajoutLivre(94,"Harry Potter Episode IV",62,"JK Rowling","fantastique","Gallimard jeunesse","IUT");
+CALL ajoutLivre(95,"Harry Potter Episode IV",62,"JK Rowling","fantastique","Gallimard jeunesse","Nokia");
+CALL ajoutLivre(96,"Harry Potter Episode IV",62,"JK Rowling","fantastique","Gallimard jeunesse","IFSI");
+CALL ajoutLivre(97,"Harry Potter Episode IV",62,"JK Rowling","fantastique","Gallimard jeunesse","Orange");
+CALL ajoutLivre(98,"Harry Potter Episode V",63,"JK Rowling","fantastique","Gallimard jeunesse","IFSI");
+CALL ajoutLivre(99,"Harry Potter Episode V",63,"JK Rowling","fantastique","Gallimard jeunesse","Orange");
+CALL ajoutLivre(100,"Harry Potter Episode V",63,"JK Rowling","fantastique","Gallimard jeunesse","Nokia");
+CALL ajoutLivre(101,"Harry Potter Episode V",63,"JK Rowling","fantastique","Gallimard jeunesse","IUT");
+CALL ajoutLivre(102,"Harry Potter Episode VI",64,"JK Rowling","fantastique","Gallimard jeunesse","IFSI");
+CALL ajoutLivre(103,"Harry Potter Episode VI",64,"JK Rowling","fantastique","Gallimard jeunesse","Orange");
+CALL ajoutLivre(104,"Harry Potter Episode VII",65,"JK Rowling","fantastique","Gallimard jeunesse","IUT");
+CALL ajoutLivre(105,"Harry Potter Episode VII",65,"JK Rowling","fantastique","Gallimard jeunesse","Nokia");
+
+CALL ajoutAbonne("THOMAS","Eliott","ethomas@enssat.fr");
+CALL ajoutAbonne("THOMAS","Julien","jthomas@enssat.fr");
+CALL ajoutAbonne("DUPOND","Bernard","bdupond@ifsi.fr");
+CALL ajoutAbonne("SKYWALKER","Rey","rskywalker@enssat.fr");
+CALL ajoutAbonne("PALPATINE","Rey","rpalpatine@enssat.fr");
+CALL ajoutAbonne("DURJARDIN","Jean","jdujardin@nokia.fr");
+CALL ajoutAbonne("BON","Jean","jbon@iut.fr");
+CALL ajoutAbonne("DURAND","Michel","mdurand@ifsi.fr");
+CALL ajoutAbonne("DARC","Jeanne","jdarc@ifsi.fr");
+CALL ajoutAbonne("DESNOYER","Aimée","adesnoyer@iut.fr");
+CALL ajoutAbonne("NADEAU","Christabel","cnadeau@orange.fr");
+CALL ajoutAbonne("LABOSSIERE","Garland","glabossiere@ifsi.fr");
+CALL ajoutAbonne("DUFRESNE","Alexandrin","adufresne@iut.fr");
+CALL ajoutAbonne("JETTE","Martin","mjette@orange.fr");
+CALL ajoutAbonne("FOUQUET","Fayette","ffouquet@nokia.fr");
+CALL ajoutAbonne("COULOMBE","Percy","pcouloumbe@enssat.fr");
+CALL ajoutAbonne("JARDINE","Octave","ojardine@ifsi.fr");
+CALL ajoutAbonne("LEROUX","Eloise","eleroux@nokia.fr");
+CALL ajoutAbonne("Raymond","Angelette","araymond@iut.fr");
+CALL ajoutAbonne("Marceau","Eliot","emarceau@orange.fr");
+
+
+
+
+
+
