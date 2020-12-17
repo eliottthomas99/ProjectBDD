aa45612afcd4cd9de166ddf9dcf8220020edbaad
Unknown <jthomas@enssat.fr>
2020-12-17 16:45:45 +0100

    Main V 3.3
    insertion jdd

---
+++ insertionJeuDeDonnees.sql
@@ -0,0 +1,23 @@
+#Ajout de contenus
+CALL ajoutEFilm(1,"Star Wars Episode I",1,"George Lucas","Rick McCallum","science fiction","ENSSAT");
+CALL ajoutEFilm(2,"Star Wars Episode I",1,"George Lucas","Rick McCallum","science fiction","ENSSAT");
+CALL ajoutEFilm(3,"Star Wars Episode I",1,"George Lucas","Rick McCallum","science fiction","ENSSAT");
+CALL ajoutEFilm(4,"Star Wars Episode II",2,"George Lucas","Rick McCallum","science fiction","ENSSAT");
+CALL ajoutEFilm(5,"Star Wars Episode III",3,"George Lucas","Rick McCallum","science fiction","ENSSAT");
+CALL ajoutEFilm(6,"Star Wars Episode III",3,"George Lucas","Rick McCallum","science fiction","ENSSAT");
+CALL ajoutEFilm(7,"Star Wars Episode IV",4,"George Lucas","Rick McCallum","science fiction","ENSSAT");
+CALL ajoutEFilm(8,"Star Wars Episode V",5,"Irvin Kershner","Rick McCallum","science fiction","ENSSAT");
+CALL ajoutEFilm(9,"Star Wars Episode VI",6,"Irvin Kershner","Rick McCallum","science fiction","ENSSAT");
+CALL ajoutEFilm(10,"Star Wars Episode VI",6,"Irvin Kershner","Rick McCallum","science fiction","ENSSAT");
+CALL ajoutEFilm(11,"Star Wars Episode VII",7,"J.J. Abrams","J.J. Abrams","science fiction","ENSSAT");
+CALL ajoutEFilm(12,"Star Wars Episode VIII",8,"Rian Johnson","Kathleen Kennedy","science fiction","ENSSAT");
+CALL ajoutEFilm(13,"Star Wars Episode VIII",8,"Rian Johnson","Kathleen Kennedy","science fiction","ENSSAT");
+CALL ajoutEFilm(14,"Star Wars Episode IX",9,"J.J. Abrams","J.J. Abrams","science fiction","ENSSAT");
+CALL ajoutEFilm(15,"Star Wars Episode IX",9,"J.J. Abrams","J.J. Abrams","science fiction","ENSSAT");
+CALL ajoutEFilm(16,"Star Wars Episode IX",9,"J.J. Abrams","J.J. Abrams","science fiction","ENSSAT");
+CALL ajoutDVD(17,"Nemo",10,"Andrew Stanton","Pixar","animation","Orange");
+
+
+
+
+/**********************************************************************************/
