9de4e1f474e0d80f868555d91bfb1779c710549c
Eliott <ethomas@enssat.fr>
2020-12-17 11:55:50 +0100

    Main V 2.29
    Ajout de l'event retrait_abonne qui retire les abonne dont l'abonnement arrive un an d'expiration et n'est pas renouvelé

--- event.sql
+++ event.sql
@@ -2,6 +2,33 @@ use bibliotheque;
 
 DELIMITER $$
 
+DROP PROCEDURE IF EXISTS proc_retrait_abonne$$ 
+CREATE PROCEDURE proc_retrait_abonne()
+# Retire les abonne qui n ont pas renouvellé leur forfait au bout d un an
+ECHEANCIER_LABEL:BEGIN
+	DELETE FROM demande  
+				WHERE (demande.Abonne_numero IN (
+                select t.numero from(
+                SELECT abonne.numero from abonne join demande on (abonne.numero=demande.Abonne_numero)
+                where datediff(curdate(),abonne.dateAdhesion)>366) as t
+                ));
+                
+                
+                
+	DELETE FROM emprunt  
+				WHERE (emprunt.Abonne_numero IN (
+                select t.numero from(
+                SELECT abonne.numero from abonne join emprunt on (abonne.numero=emprunt.Abonne_numero)
+                where datediff(curdate(),abonne.dateAdhesion)>366) as t
+                ));
+	DELETE FROM abonne  
+				WHERE (abonne.numero IN (
+                select t.numero from(
+                SELECT abonne.numero from abonne
+                where datediff(curdate(),abonne.dateAdhesion)>366) as t
+                ));
+END;
+$$
 
 
 /* E au dessus, J en dessous */
@@ -10,13 +37,14 @@ DELIMITER $$
 DELIMITER ;
 
 
-DROP EVENT IF EXISTS e_hourly;
-CREATE EVENT e_hourly
+DROP EVENT IF EXISTS retrait_abonne;
+CREATE EVENT retrait_abonne
     ON SCHEDULE
       EVERY 1 MINUTE
-    COMMENT 'Clears out sessions table each hour.'
+    COMMENT 'Retire les abonne qui n ont pas renouvellé leur forfait au bout d un an'
     DO
-      DELETE FROM `bibliotheque`.`possede` WHERE (`Contenu_Code_Barre` = '0000000040') and (`Contenu_Numero_License` = '0000000000') and (`Etablissement_id` = '7');
+      CALL proc_retrait_abonne();
+
 
 
 
