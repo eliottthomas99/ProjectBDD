9c67cb79671b8f384a34c6b5d18569e083403903
Unknown <jthomas@enssat.fr>
2020-12-17 12:17:13 +0100

    Main V 2.30
    implementation procedure supprimerDemandesDepassees
    implementation event meangeDemandeDepassees

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ## problemes
 * possibilite d emprunter plusieurs contenu avec le meme code catalogue
 * une meme personne ne peut pas emprunter 2 fois le meme contenu une meme journee
+* contenu emprunte lorsque abonnement arrive a sa fin ?
--- event.sql
+++ event.sql
@@ -33,6 +33,41 @@ $$
 
 /* E au dessus, J en dessous */
 
+DROP PROCEDURE IF EXISTS supprimerDemandesDepassees$$ 
+CREATE PROCEDURE supprimerDemandesDepassees()
+# supprime toutes les demandes concernant un code catalogue qui a ete retourne il y plus de 7 jours
+supprimerDemandesDepassees:BEGIN
+/*
+	SET @codeCatalogueRetourPlusDe7j = (SELECT DISTINCT codeCatalogue FROM contenu
+										JOIN emprunt 
+											ON Contenu.Code_Barre = emprunt.Contenu_Code_Barre AND Contenu.Numero_License = emprunt.Contenu_Numero_License
+										WHERE date_retour IS NOT NULL
+                                        ORDER BY date_retour DESC); */
+										
+	#DELETE FROM demande WHERE ((Contenu_Code_Barre, Contenu_Numero_License) IN (SELECT Code_Barre,Numero_License FROM contenu WHERE codeCatalogue = @codeCatalogue)) AND Abonne_numero = abonneValNumero;
+	
+DELETE FROM demande
+WHERE (Contenu_Code_Barre, Contenu_Numero_License) IN (# retourne les code_barre et numero_license des demandes a supprimer
+														SELECT DISTINCT Code_Barre, Numero_License FROM contenu
+																							JOIN emprunt 
+																								ON Contenu.Code_Barre = emprunt.Contenu_Code_Barre AND Contenu.Numero_License = emprunt.Contenu_Numero_License
+																							# On regarde si le code catalogue fait parti des codes catalogues dispo a l emprunt
+																							WHERE codeCatalogue IN (SELECT codeCatalogue FROM contenu AS contenu1
+																																			JOIN emprunt AS emprunt1
+																																				ON contenu1.Code_Barre = emprunt1.Contenu_Code_Barre AND Contenu1.Numero_License = emprunt1.Contenu_Numero_License
+																																		# On regarde si l emprunt le plus recent est suivi d un rendu
+																																		WHERE date_pret = ( SELECT MAX(date_pret) FROM (contenu AS contenu2
+																																													JOIN emprunt AS emprunt2
+																																														ON Contenu2.Code_Barre = emprunt2.Contenu_Code_Barre AND Contenu2.Numero_License = emprunt2.Contenu_Numero_License)
+																																													WHERE Contenu1.Code_Barre = Contenu2.Code_Barre AND Contenu1.Numero_License = Contenu2.Numero_License)
+																																		AND date_retour IS NOT NULL AND DATEDIFF(CURDATE(),date_retour) > 7)
+                                                                                
+                                                                                );
+
+
+
+END;
+$$
 
 DELIMITER ;
 
@@ -45,6 +80,15 @@ CREATE EVENT retrait_abonne
     DO
       CALL proc_retrait_abonne();
 
+
+
+DROP EVENT IF EXISTS meangeDemandeDepassees;
+CREATE EVENT meangeDemandeDepassees
+    ON SCHEDULE
+      EVERY 1 MINUTE
+    COMMENT 'Clears out demande depassees each hour.'
+    DO
+      CALL supprimerDemandesDepassees();
 
 
 
